143f2fa6daf819c5b85cbdc2216e6a4fbc896244
Kartik <kartik.arora1214@gmail.com>
2022-05-05 16:57:14 +0530

    add showShortCut prop and conditional titles

--- src/components/md-toolbar-left.vue
+++ src/components/md-toolbar-left.vue
@@ -3,13 +3,13 @@
         <slot name="left-toolbar-before" />
         <button :disabled="!editable" type="button" v-if="toolbars.bold" @click="$clicks('bold')"
                 class="op-icon fa fa-mavon-bold" aria-hidden="true"
-                :title="`${d_words.tl_bold} (ctrl+b)`"></button>
+                :title="`${d_words.tl_bold} ${showShortCut ? '(ctrl+b)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.italic" @click="$clicks('italic')"
                 class="op-icon fa fa-mavon-italic" aria-hidden="true"
-                :title="`${d_words.tl_italic} (ctrl+i)`"></button>
+                :title="`${d_words.tl_italic} ${showShortCut ? '(ctrl+i)' : ''}`"></button>
         <div :class="{'selected': s_header_dropdown_open}" :disabled="!editable" type="button" v-if="toolbars.header" @mouseleave="$mouseleave_header_dropdown" @mouseenter="$mouseenter_header_dropdown"
                 class="op-icon fa fa-mavon-header dropdown dropdown-wrapper" aria-hidden="true"
-                :title="`${d_words.tl_header} (ctrl+h)`">
+                :title="`${d_words.tl_header} ${showShortCut ? '(ctrl+h)' : ''}`">
             <transition name="fade">
                 <div class="op-header popup-dropdown" :class="{'transition': transition}" v-show="s_header_dropdown_open" @mouseenter="$mouseenter_header_dropdown" @mouseleave="$mouseleave_header_dropdown">
                     <div title="#"  class="dropdown-item" @click.stop="$click_header('header1')"><span>{{d_words.tl_header_one}}</span></div>
@@ -24,45 +24,45 @@
         <span v-if="toolbars.header || toolbars.italic || toolbars.bold" class="op-icon-divider"></span>
         <button :disabled="!editable" type="button" v-if="toolbars.underline" @click="$clicks('underline')"
                 class="op-icon fa fa-mavon-underline"
-                :title="`${d_words.tl_underline} (ctrl+u)`" aria-hidden="true"></button>
+                :title="`${d_words.tl_underline} ${showShortCut ? '(ctrl+u)' : ''}`" aria-hidden="true"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.strikethrough" @click="$clicks('strikethrough')"
                 class="op-icon fa fa-mavon-strikethrough"
-                :title="`${d_words.tl_strikethrough} (ctrl+shift+d)`" aria-hidden="true"></button>
+                :title="`${d_words.tl_strikethrough} ${showShortCut ? '(ctrl+shift+d)' : ''}`" aria-hidden="true"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.mark" @click="$clicks('mark')"
-                class="op-icon fa fa-mavon-thumb-tack" :title="`${d_words.tl_mark} (ctrl+m)`"
+                class="op-icon fa fa-mavon-thumb-tack" :title="`${d_words.tl_mark} ${showShortCut ? '(ctrl+m)' : ''}`"
                 aria-hidden="true"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.superscript" @click="$clicks('superscript')"
                 class="op-icon fa fa-mavon-superscript"
-                aria-hidden="true" :title="`${d_words.tl_superscript} (ctrl+alt+s)`"></button>
+                aria-hidden="true" :title="`${d_words.tl_superscript} ${showShortCut ? '(ctrl+alt+s)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.subscript" @click="$clicks('subscript')"
                 class="op-icon fa fa-mavon-subscript"
-                aria-hidden="true" :title="`${d_words.tl_subscript} (ctrl+shift+s)`"></button>
+                aria-hidden="true" :title="`${d_words.tl_subscript} ${showShortCut ? '(ctrl+shift+s)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.alignleft" @click="$clicks('alignleft')"
                 class="op-icon fa fa-mavon-align-left"
-                aria-hidden="true" :title="`${d_words.tl_alignleft} (ctrl+l)`">
+                aria-hidden="true" :title="`${d_words.tl_alignleft} ${showShortCut ? '(ctrl+l)' : ''}`">
         </button>
         <button :disabled="!editable" type="button" v-if="toolbars.aligncenter" @click="$clicks('aligncenter')"
                 class="op-icon fa fa-mavon-align-center"
-                aria-hidden="true" :title="`${d_words.tl_aligncenter} (ctrl+e)`"></button>
+                aria-hidden="true" :title="`${d_words.tl_aligncenter} ${showShortCut ? '(ctrl+e)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.alignright" @click="$clicks('alignright')"
                 class="op-icon fa fa-mavon-align-right"
-                aria-hidden="true" :title="`${d_words.tl_alignright} (ctrl+r)`"></button>
+                aria-hidden="true" :title="`${d_words.tl_alignright} ${showShortCut ? '(ctrl+r)' : ''}`"></button>
         <span
                 v-if="toolbars.superscript || toolbars.subscript || toolbars.underline || toolbars.strikethrough || toolbars.mark"
                 class="op-icon-divider"></span>
         <button :disabled="!editable" type="button" v-if="toolbars.quote" @click="$clicks('quote')"
                 class="op-icon fa fa-mavon-quote-left" aria-hidden="true"
-                :title="`${d_words.tl_quote} (ctrl+q)`"></button>
+                :title="`${d_words.tl_quote} ${showShortCut ? '(ctrl+q)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.ol" @click="$clicks('ol')"
                 class="op-icon fa fa-mavon-list-ol" aria-hidden="true"
-                :title="`${d_words.tl_ol} (ctrl+o)`"></button>
+                :title="`${d_words.tl_ol} ${showShortCut ? '(ctrl+o)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.ul" @click="$clicks('ul')"
                 class="op-icon fa fa-mavon-list-ul" aria-hidden="true"
-                :title="`${d_words.tl_ul} (ctrl+alt+u)`"></button>
+                :title="`${d_words.tl_ul} ${showShortCut ? '(ctrl+alt+u)' : ''}`"></button>
         <span v-if="toolbars.ul || toolbars.ol || toolbars.quote" class="op-icon-divider"></span>
         <button :disabled="!editable" type="button" v-if="toolbars.link" @click.stop="$toggle_imgLinkAdd('link')"
                 class="op-icon fa fa-mavon-link" aria-hidden="true"
-                :title="`${d_words.tl_link} (ctrl+l)`"></button>
+                :title="`${d_words.tl_link} ${showShortCut ? '(ctrl+l)' : ''}`"></button>
         <div :disabled="!editable" :class="{'selected': s_img_dropdown_open}" type="button" v-if="toolbars.imagelink" @mouseleave="$mouseleave_img_dropdown" @mouseenter="$mouseenter_img_dropdown"
                 class="op-icon fa fa-mavon-picture-o dropdown dropdown-wrapper"
                 aria-hidden="true">
@@ -93,23 +93,23 @@
         </div>
         <button :disabled="!editable" type="button" v-if="toolbars.code" @click="$clicks('code')"
                 class="op-icon fa fa-mavon-code" aria-hidden="true"
-                :title="`${d_words.tl_code} (ctrl+alt+c)`"></button>
+                :title="`${d_words.tl_code} ${showShortCut ? '(ctrl+alt+c)' : ''}`"></button>
         <button :disabled="!editable" type="button" v-if="toolbars.table" @click="$clicks('table')"
                 class="op-icon fa fa-mavon-table" aria-hidden="true"
-                :title="`${d_words.tl_table} (ctrl+alt+t)`"></button>
+                :title="`${d_words.tl_table} ${showShortCut ? '(ctrl+alt+t)' : ''}`"></button>
         <span v-if="toolbars.link || toolbars.imagelink || toolbars.code || toolbars.table"
               class="op-icon-divider"></span>
         <button type="button" v-if="toolbars.undo" @click="$clicks('undo')" class="op-icon fa fa-mavon-undo"
-                aria-hidden="true" :title="`${d_words.tl_undo} (ctrl+z)`"></button>
+                aria-hidden="true" :title="`${d_words.tl_undo} ${showShortCut ? '(ctrl+z)' : ''}`"></button>
         <button type="button" v-if="toolbars.redo" @click="$clicks('redo')" class="op-icon fa fa-mavon-repeat"
                 aria-hidden="true"
-                :title="`${d_words.tl_redo} (ctrl+y)`"></button>
+                :title="`${d_words.tl_redo} ${showShortCut ? '(ctrl+y)' : ''}`"></button>
         <button type="button" v-if="toolbars.trash" @click="$clicks('trash')" class="op-icon fa fa-mavon-trash-o"
                 aria-hidden="true"
-                :title="`${d_words.tl_trash} (ctrl+breakspace)`"></button>
+                :title="`${d_words.tl_trash} ${showShortCut ? '(ctrl+breakspace)' : ''}`"></button>
         <button type="button" v-if="toolbars.save" @click="$clicks('save')" class="op-icon fa fa-mavon-floppy-o"
                 aria-hidden="true"
-                :title="`${d_words.tl_save} (ctrl+s)`"></button>
+                :title="`${d_words.tl_save} ${showShortCut ? '(ctrl+s)' : ''}`"></button>
         <slot name="left-toolbar-after" />
 
         <!-- 添加image链接 -->
@@ -155,6 +155,10 @@
             image_filter: {
                 type: Function,
                 default: null
+            },
+            showShortCut: {
+                type: Boolean,
+                default: true
             }
         },
         data() {
